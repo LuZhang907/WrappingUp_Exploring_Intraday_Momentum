fa7491d0db124854751c4acc408fa38541a2a101
LuZhang907 <63075429+LuZhang907@users.noreply.github.com>
2023-10-06 10:42:10 -0500

    vis correaltion update

---
+++ Wavelet Transformation/correlation_vis.R
@@ -0,0 +1,10 @@
+rm(list = setdiff(ls(), lsf.str()))
+library(data.table)
+library(PerformanceAnalytics)
+
+
+dat <- fread("/Users/luzhang/Documents/GitHub/WrappingUp_Exploring_Intraday_Momentum_2rd_try/Data/SPY_nscoreReturns.csv")
+dat <- data.frame(dat)
+head(dat)
+
+chart.Correlation(dat,histogram = FALSE)
