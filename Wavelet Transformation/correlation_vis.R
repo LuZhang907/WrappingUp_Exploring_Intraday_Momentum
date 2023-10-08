dc75d191a3ef6957247224d9f0f5cf827e2c6592
LuZhang907 <63075429+LuZhang907@users.noreply.github.com>
2023-10-07 19:50:22 -0500

    update

--- Wavelet Transformation/correlation_vis.R
+++ Wavelet Transformation/correlation_vis.R
@@ -3,7 +3,7 @@ library(data.table)
 library(PerformanceAnalytics)
 
 
-dat <- fread("/Users/luzhang/Documents/GitHub/WrappingUp_Exploring_Intraday_Momentum_2rd_try/Data/AMZN_nscoreReturns.csv")
+dat <- fread("/Users/luzhang/Documents/GitHub/WrappingUp_Exploring_Intraday_Momentum_2rd_try/Data/GOOGL_nscoreReturns.csv")
 dat <- data.frame(dat)
 head(dat)
 
